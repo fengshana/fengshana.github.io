762fefd630ae674e209ea25ac218fef64815fe65
Fsn <2915324940@qq.com>
2020-02-17 22:21:19 +0800

    Site updated: 2020-02-17 22:20:41

---
+++ dist/music.js
@@ -0,0 +1,85 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: '孽海记',
+        artist: '黄诗扶',
+        url: 'http://music.163.com/song/media/outer/url?id=1370879975.mp3',
+        cover: 'http://p1.music.126.net/a5NVmn_xa9QAoXE46n7SJw==/109951164137683050.jpg',
+      },
+	{
+        name: '清欢年岁',
+        artist: '以冬',
+        url: 'http://music.163.com/song/media/outer/url?id=500885418.mp3',
+        cover: 'http://p1.music.126.net/YDILDjSTE-MBuxQ-k9l22Q==/109951163009194055.jpg',
+      },
+	{
+        name: '未见青山老',
+        artist: '以冬',
+        url: 'http://music.163.com/song/media/outer/url?id=453175619.mp3',
+        cover: 'http://p1.music.126.net/Bli08kedCa1pT0p05AxO8A==/109951162955789816.jpg',
+      },
+	{
+        name: '与你相遇这世界',
+        artist: '泥鳅Niko',
+        url: 'http://music.163.com/song/media/outer/url?id=488117651.mp3',
+        cover: 'http://p1.music.126.net/5nRc0whORRbgCuqrCJPTcA==/109951162963857494.jpg',
+      },
+	{
+        name: '打上花火',
+        artist: 'DAOKO / 米津玄師',
+        url: 'http://music.163.com/song/media/outer/url?id=496869422.mp3',
+        cover: 'http://p2.music.126.net/ZUCE_1Tl_hkbtamKmSNXEg==/109951163009282836.jpg',
+      },
+	{
+        name: 'Eudemon树林与鹿的对话',
+        artist: '凯瑟喵',
+        url: 'http://music.163.com/song/media/outer/url?id=1388351993.mp3',
+        cover: 'http://p2.music.126.net/J3AsA6QDXjJgqEzGyLTGpQ==/109951164341600947.jpg',
+      },
+	{
+        name: '我不能悲伤地坐在你身旁',
+        artist: '左小祖咒',
+        url: 'http://music.163.com/song/media/outer/url?id=190119.mp3',
+        cover: 'http://p1.music.126.net/SW31z_f91o85bB2zoP4kCg==/25288767450719.jpg',
+      },
+	{
+        name: 'Illusionary Daytime幻昼',
+        artist: 'Shirfine',
+        url: 'http://music.163.com/song/media/outer/url?id=28907016.mp3',
+        cover: 'http://p2.music.126.net/8xNVCemkSNQptEyNw1PHKg==/8914840278033758.jpg',
+      },
+	  {
+        name: '吹灭小山河',
+        artist: '国风堂 / 司南',
+        url: 'http://music.163.com/song/media/outer/url?id=1412559986.mp3',
+        cover: 'http://p2.music.126.net/cZolbvntU90ECMSR_lVF9Q==/109951164583584578.jpg',
+      },
+	   {
+        name: '长安的少年',
+        artist: '国风堂 / 小魂',
+        url: 'http://music.163.com/song/media/outer/url?id=1391439603.mp3',
+        cover: 'http://p1.music.126.net/zx0Ij4Df3-V6V4pw_EO1JA==/109951164370004358.jpg',
+      },
+	    {
+        name: '盗墓笔记 流离 (无念白)',
+        artist: 'Winky诗',
+        url: 'http://music.163.com/song/media/outer/url?id=34364773.mp3',
+        cover: 'http://p2.music.126.net/zYz65Dm6KbLu8-2C56qpKw==/25288767439902.jpg',
+      },
+      {
+        name: "招摇",
+        artist: '陈楚生/胡莎莎',
+        url: 'http://music.163.com/song/media/outer/url?id=1343200598.mp3',
+        cover: 'http://p2.music.126.net/760ZgRAzDD8An9ob9UGRBg==/109951163815997796.jpg',
+      },
+      {
+        name: '千秋迭梦——《二哈和他的白猫师尊》燃晚同人曲',
+        artist: '镜予歌 / 陈亦洺 / _尚辰_',
+        url: 'http://music.163.com/song/media/outer/url?id=1382865496.mp3',
+        cover: 'http://p1.music.126.net/ICrF78E04Wk3EziDzWKBHA==/109951164275168532.jpg',
+      }
+    ]
+});
