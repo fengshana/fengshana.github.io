572c79a044031b1f9341bb04ed7cbdda7bcc7307
Fsn <2915324940@qq.com>
2020-05-21 18:11:40 +0800

    Site updated: 2020-05-21 18:10:35

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -9,7 +9,7 @@
       $(".highlight .code pre").before(copyHtml);
       new ClipboardJS('.btn-copy', {
           target: function(trigger) {
-              $('<div>').appendTo('body').addClass('alert alert-success').html('道友ᕦ(･ㅂ･)ᕤ，复制成功了哟(^_−)☆').show().delay(1500).fadeOut();
+              $('<div>').appendTo('body').addClass('alert alert-success').html('🤠道友ᕦ(･ㅂ･)ᕤ，🥭复制成功了哟(^_−)☆').show().delay(1500).fadeOut();
               return trigger.nextElementSibling;
             }
       });
